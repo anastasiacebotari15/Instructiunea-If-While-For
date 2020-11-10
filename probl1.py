90c241e8a040211401a3230b06ce24263dc185a3
Cebotari Anastasia <71016019+anastasiacebotari15@users.noreply.github.com>
2020-11-10 10:09:02 +0300

    Add files via upload

---
+++ probl1.py
@@ -0,0 +1,9 @@
+n=int(input("n="))
+if ((n==28) or (n==29)):
+    print("februarie")
+elif (n==30):
+    print("septembrie, noiembrie, aprilie, iunie")
+elif (n==31):
+    print ("ianuarie, martie, mai, iulie, august, octombrie, decembrie")
+else:
+    print("nu exista asa luna")
